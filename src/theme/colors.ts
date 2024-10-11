9425a29aa6de85d84af260da67299b4413244699
MauriNestor <mauric845apaza@gmail.com>
2024-10-10 23:42:53 -0400

    component: text

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+    primary: '#203ED5',
+    secondary: '#B1BBC6',
+    white: '#FFFFFF',
+    muted: '#788298',
+    dark: '#07122F',
+    black: '#000000',
+    black_a40: 'rgba(0, 0, 0, 0.4)',
+    lowlight: '#D9DEE4',
+    red: '#FF0000',
+  };
