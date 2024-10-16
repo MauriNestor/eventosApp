d19e7c7e49dd121b8cd0994bd3ba915c0e28952d
MauriNestor <mauric845apaza@gmail.com>
2024-10-15 23:52:39 -0400

    add home screen

---
+++ src/screens/app/home/homeScreen.tsx
@@ -0,0 +1,42 @@
+import { ScrollView, StyleSheet } from 'react-native'
+import React from 'react'
+
+import SectionContainer from '@app/components/molecules/SectionContainer';
+import CategoriesList from './components/CategoriesList';
+import EventList from './components/EventList';
+
+import { colors } from '@app/theme/colors';
+
+const HomeScreen = () => {
+
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.content}
+    >
+      <SectionContainer title='Categorías'>
+        <CategoriesList  />
+      </SectionContainer>
+
+      <SectionContainer title='Películas'>
+        <EventList />
+      </SectionContainer>
+
+    </ScrollView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: colors.white,
+  },
+  content: {
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+});
