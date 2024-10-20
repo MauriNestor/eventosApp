6788ee633203c17288511993316173b1b5ba7bd5
MauriNestor <mauric845apaza@gmail.com>
2024-10-20 19:43:34 -0400

    add text componenet

--- src/components/TextComponent.tsx
+++ src/components/TextComponent.tsx
@@ -35,6 +35,7 @@ const TextComponent = (props: Props) => {
         props.color === 'dark' && styles.darkColor,
         props.color === 'muted' && styles.mutedColor,
         props.color === 'white' && styles.whiteColor,
+        props.customColor && { color: props.color },
         props.textAlign === 'left' && styles.left,
         props.textAlign === 'center' && styles.center,
         props.textAlign === 'right' && styles.right,
