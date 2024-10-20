be707da456371a8b24aad0c41c6f99eb162777ce
MauriNestor <mauric845apaza@gmail.com>
2024-10-20 19:44:29 -0400

    welcomeScreen uptaded

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,74 +1,91 @@
-import {SafeAreaView, StyleSheet} from 'react-native'
-import { NavigationProp } from '@react-navigation/native';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native'
+// import { NavigationProp } from '@react-navigation/native';
 
-import TextComponent from '../components/atoms/TextComponent';
-import TextInputComponent from '../components/atoms/TextInputComponent';
-import { useState } from 'react';
 
-type Props = {
-  navigation: NavigationProp<any>,
-}
+import TextComponent from '../components/TextComponent';
+// import TextInputComponent from '../components/atoms/TextInputComponent';
+// import { useState } from 'react';
 
-const WelcomeScreen = ({ navigation }: Props) => {
-  const [input, setInput] = useState<string>('')
+// type Props = {
+//   navigation: NavigationProp<any>,
+// }
 
+// const WelcomeScreen = ({ navigation }: Props) => {
+//   const [input, setInput] = useState<string>('')
+
+
+//   const handleOnPressButton = () => {
+//     // navigation.navigate('EventDetailScreen')
+//     // navigation.navigate('EventDetailScreen', {
+//     //   name: 'abdiel',
+//     //   lastName: "Orellana",
+//     //   course: "Expo",
+//     // })
+//   };
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <TextComponent
+//         size='24'
+//         weight='light'
+//         color='muted'
+//         textAlign='center'
+//       >
+//         Welcome to the App!
+//         <TextComponent
+//           size='24'
+//           weight='bold'
+//           color='primary'
+//           underline={true}
+//           onPress={handleOnPress}
+//         >
+//           Texto en negrita
+//         </TextComponent>
+//       </TextComponent>
+
+//       <TextComponent
+//           size='24'
+//           weight='bold'
+//           color='dark'
+//           // underline={true}
+//           // onPress={handleOnPress}
+//         >
+//           {`Input value ${input}`}
+//         </TextComponent>
+
+//       <TextInputComponent
+//         placeholder='Ingrese Texto'
+//         value={input}
+//         onChangeText={(text) => setInput(text)}
+//       />
+//       {/* <ButtonComponent
+//         title='Ir al detalle del evento!'
+//         disabled={false}
+//         onPress={handleOnPressButton}
+//       /> */}
+//     </SafeAreaView>
+//   )
+// }
+
+
+
+
+const WelcomeScreen = () => {
   const handleOnPress = () => {
     console.log('texto en negrita presionado')
-  };
-
-  const handleOnPressButton = () => {
-    // navigation.navigate('EventDetailScreen')
-    // navigation.navigate('EventDetailScreen', {
-    //   name: 'abdiel',
-    //   lastName: "Orellana",
-    //   course: "Expo",
-    // })
-  };
-
+  }
   return (
-    <SafeAreaView style={styles.container}>
-      <TextComponent
-        size='24'
-        weight='light'
-        color='muted'
-        textAlign='center'
-      >
+    <SafeAreaView>
+      <TextComponent size='18' weight='light' color='muted' textAlign='center'>
         Welcome to the App!
-        <TextComponent
-          size='24'
-          weight='bold'
-          color='primary'
-          underline={true}
-          onPress={handleOnPress}
-        >
-          Texto en negrita
-        </TextComponent>
+        <TextComponent size='24' weight='light' color='dark' onPress={handleOnPress} >texto negrita</TextComponent>
       </TextComponent>
 
-      <TextComponent
-          size='24'
-          weight='bold'
-          color='dark'
-          // underline={true}
-          // onPress={handleOnPress}
-        >
-          {`Input value ${input}`}
-        </TextComponent>
-
-      <TextInputComponent
-        placeholder='Ingrese Texto'
-        value={input}
-        onChangeText={(text) => setInput(text)}
-      />
-      {/* <ButtonComponent
-        title='Ir al detalle del evento!'
-        disabled={false}
-        onPress={handleOnPressButton}
-      /> */}
+      <Button title='Ir al detalle del evento!' onPress={() => console.log('ir al detalle del evento')} />
+  
     </SafeAreaView>
-  )
+  );
 }
-
 export default WelcomeScreen;
 
 const styles = StyleSheet.create({
@@ -78,10 +95,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  // TODO: estos estilos de texto ya no son requeridos, se eliminaran proximanente
   text: {
     fontSize: 24,
-    fontWeight: '400',
+    fontWeight: '600',
   },
   textoNegrita: {
     fontSize: 24,
